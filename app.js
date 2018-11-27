a8c973b50a81c0d432c63dcbc9bc78ca379cc114
Ammar <ammar.alnatsheh@gmail.com>
2018-11-26 23:05:07 -0800

    finish the min req

--- app.js
+++ app.js
@@ -1,1 +1,24 @@
 // TODO
+var TodoList = (props) => {
+
+  var onListItemClick = (event) => {
+    console.log('Finish doing this task');
+  };
+
+  return (
+    <ul>
+      <li onClick={onListItemClick}>{props.todos[0]}</li>
+      <li onClick={onListItemClick}>{props.todos[1]}</li>
+      <li onClick={onListItemClick}>{props.todos[2]}</li>
+    </ul>
+  );
+}
+
+var App = () => (
+  <div>
+    <h2>My Todo List</h2>
+    <TodoList todos={['Learn React', 'Crush Recast.ly', 'Maybe sleep']}/>
+  </div>
+);
+
+ReactDOM.render(<App />, document.getElementById('app'));
